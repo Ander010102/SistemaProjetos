fec4b949389859bf1b52a42cb7152030a0d99db6
Anderson Carneiro dos Santos <anderson.carneiro0101@gmail.com>
2026-09-04 10:03:10 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("====================================");
+        System.out.println("     SISTEMAS DE PROJETOS");
+        System.out.println("====================================");
+        System.out.println("Bem vindo ao sistema!");
+    }
+}
