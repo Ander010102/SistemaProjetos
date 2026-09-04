b64d280c15419a9c1e52e208d5a3bd17dcc24a6a
Anderson Carneiro dos Santos <anderson.carneiro0101@gmail.com>
2026-09-04 10:11:17 -0300

    Add files via upload

---
+++ AULA1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("====================================");
+        System.out.println("     SISTEMAS DE PROJETOS");
+        System.out.println("====================================");
+        System.out.println("Projeto: Portifólio Acadêmico");
+        System.out.println("Desenvolvimento em Java");
+        System.out.println("Versão: 1.0");
+
+    }
+}
