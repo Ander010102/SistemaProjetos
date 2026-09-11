15ba358fed953fa10502d5bd07ea1171c97a286a
Anderson Carneiro dos Santos <anderson.carneiro0101@gmail.com>
2026-09-11 10:03:20 -0300

    Update Projeto.java

--- AULA1/src/model/Projeto.java
+++ AULA1/src/model/Projeto.java
@@ -7,3 +7,42 @@ public class Projeto {
     private String categoria;
     private String status;
 }
+
+public int getId(){
+        return id;
+    }
+     public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
