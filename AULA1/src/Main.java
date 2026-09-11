f881af291da4e959f1b6de0ab883bc6befc166b3
Anderson Carneiro dos Santos <anderson.carneiro0101@gmail.com>
2026-09-11 10:07:58 -0300

    Update Main.java

--- AULA1/src/Main.java
+++ AULA1/src/Main.java
@@ -1,13 +1,32 @@
+import model.Projeto;
+
 public class Main
 {
     public static void main(String[] args)
-    {
-        System.out.println("====================================");
-        System.out.println("     SISTEMAS DE PROJETOS");
-        System.out.println("====================================");
-        System.out.println("Projeto: Portifólio Acadêmico");
-        System.out.println("Desenvolvimento em Java");
-        System.out.println("Versão: 1.0");
+    Projeto projeto = new Projeto();
+    projeto.setId(1);
+    projeto.setNome("Sistema Acadêmico");
+    projeto.setDescricao("Sistema para gerenciamento acadêmico");
+    projeto.setCategoria("Software");
+    projeto.setStatus("Em desenvolvimento");
 
+    System.out.println(projeto.getId());
+    System.out.println(projeto.getNome());
+    System.out.println(projeto.getDescricao());
+    System.out.println(projeto.getCategoria());
+    System.out.println(projeto.getStatus());
+
+
+    {
+        System.out.println("==============================");
+        System.out.println("    SISTEMA DE PROJETOS");
+        System.out.println("==============================");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Projeto: Portfólio Acadêmico");
+        System.out.println("Desenvolvido em Java");
+        System.out.println("Versão 1.0");
+        
     }
 }
