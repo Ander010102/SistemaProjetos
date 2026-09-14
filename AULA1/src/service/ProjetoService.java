2235750cce4545a8a84fc972b8051862ac63e391
Anderson Carneiro dos Santos <anderson.carneiro0101@gmail.com>
2026-09-14 10:28:20 -0300

    Update ProjetoService.java

--- AULA1/src/service/ProjetoService.java
+++ AULA1/src/service/ProjetoService.java
@@ -1,1 +1,23 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Projeto;
+
+public class ProjetoService {
+
+    private List<Projeto> projetos;
+
+    public ProjetoService() {
+
+        projetos = new ArrayList<>();
+
+    }
+
+    public void adicionar(Projeto projeto) {
+        projeto.add(projeto);
+    }
+
+}
 
